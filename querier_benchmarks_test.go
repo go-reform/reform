2c471042f143a03ecec4a4a7e998b00ed5b31853
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-12-09 21:58:34 +0300

    Benchmark for Update.

--- querier_benchmarks_test.go
+++ querier_benchmarks_test.go
@@ -12,10 +12,13 @@ import (
 	. "gopkg.in/reform.v1/internal/test/models"
 )
 
+var err error
+
+const N = 10000
+
 // FindByPrimaryKeyFrom
 
 func benchmarkFindByPrimaryKeyFrom(tb testing.TB, tx *reform.TX, n int) {
-	var err error
 	for i := 0; i < n; i++ {
 		_, err = tx.FindByPrimaryKeyFrom(PersonTable, 1)
 		if err != nil {
@@ -28,7 +31,6 @@ func TestBenchmarkFindByPrimaryKeyFrom(t *testing.T) {
 	tx := setupTransaction(t, false)
 	defer tearDownTransaction(t, tx)
 
-	const N = 10000
 	start := time.Now()
 	benchmarkFindByPrimaryKeyFrom(t, tx, N)
 	t.Logf("N = %d in %s", N, time.Now().Sub(start))
@@ -46,7 +48,6 @@ func BenchmarkFindByPrimaryKeyFrom(b *testing.B) {
 // Insert
 
 func benchmarkInsert(tb testing.TB, tx *reform.TX, n int, newEmail string) {
-	var err error
 	for i := 0; i < n; i++ {
 		err = tx.Insert(&Person{Email: &newEmail})
 		if err != nil {
@@ -61,7 +62,6 @@ func TestBenchmarkInsert(t *testing.T) {
 
 	newEmail := faker.Internet().Email()
 
-	const N = 10000
 	start := time.Now()
 	benchmarkInsert(t, tx, N, newEmail)
 	t.Logf("N = %d in %s", N, time.Now().Sub(start))
@@ -75,5 +75,55 @@ func BenchmarkInsert(b *testing.B) {
 
 	b.ResetTimer()
 	benchmarkInsert(b, tx, b.N, newEmail)
+	b.StopTimer()
+}
+
+// Update
+
+func benchmarkUpdate(tb testing.TB, tx *reform.TX, n int, person *Person) {
+	email1 := "email1"
+	email2 := "email2"
+
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			person.Email = &email1
+		} else {
+			person.Email = &email2
+		}
+
+		err = tx.Update(person)
+		if err != nil {
+			tb.Fatal(err)
+		}
+	}
+}
+
+func TestBenchmarkUpdate(t *testing.T) {
+	tx := setupTransaction(t, false)
+	defer tearDownTransaction(t, tx)
+
+	person := &Person{ID: 1}
+	err := tx.Reload(person)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	start := time.Now()
+	benchmarkUpdate(t, tx, N, person)
+	t.Logf("N = %d in %s", N, time.Now().Sub(start))
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	tx := setupTransaction(b, false)
+	defer tearDownTransaction(b, tx)
+
+	person := &Person{ID: 1}
+	err = tx.Reload(person)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	benchmarkUpdate(b, tx, b.N, person)
 	b.StopTimer()
 }

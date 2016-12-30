bb3a3bb575f9e1143d2e31ae5adaceb4d87a29f2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-12-30 11:15:22 +0300

    Simplify code.

--- reform/main.go
+++ reform/main.go
@@ -74,11 +74,7 @@ func processFile(path, file, pack string) error {
 		}
 	}
 
-	if err = initTemplate.Execute(f, sds); err != nil {
-		return err
-	}
-
-	return nil
+	return initTemplate.Execute(f, sds)
 }
 
 func gofmt(path string) {
